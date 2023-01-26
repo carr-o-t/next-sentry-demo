f4f1641958ac059a801c48b7c54861a7ddc17917
carr-o-t <pandeystakshi@gmail.com>
2023-01-26 18:48:48 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 // with Sentry.
 // https://nextjs.org/docs/api-reference/next.config.js/introduction
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-const { withSentryConfig } = require('@sentry/nextjs');
+const { withSentryConfig } = require("@sentry/nextjs");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -13,10 +13,10 @@ const nextConfig = {
   },
 };
 
-module.exports = withSentryConfig(nextConfig);
+module.exports = nextConfig;
 
 module.exports = withSentryConfig(
   module.exports,
   { silent: true },
-  { hideSourcemaps: true },
+  { hideSourcemaps: true }
 );
